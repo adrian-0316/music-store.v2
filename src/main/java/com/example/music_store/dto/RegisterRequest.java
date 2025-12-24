dea27e2a2a7a7767d13da01d5bfae87b82d47df2
Adrian <112898743+adrian-0316@users.noreply.github.com>
2025-12-24 12:41:54 +0300

    dto class added

---
+++ src/main/java/com/example/music_store/dto/RegisterRequest.java
@@ -0,0 +1,17 @@
+package com.example.music_store.dto;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record RegisterRequest(
+
+        @Email
+        @NotBlank
+        String email,
+
+        @NotBlank
+        @Size(min = 6)
+        String password
+) {}
