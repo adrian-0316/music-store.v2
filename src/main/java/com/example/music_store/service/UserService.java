51da01a5ae6d3077bb47beda8c24df2698689009
Adrian <112898743+adrian-0316@users.noreply.github.com>
2025-12-27 01:43:50 +0300

    service class added

---
+++ src/main/java/com/example/music_store/service/UserService.java
@@ -0,0 +1,31 @@
+package com.example.music_store.service;
+
+import com.example.music_store.dto.RegisterRequest;
+import com.example.music_store.entity.Role;
+import com.example.music_store.entity.User;
+import com.example.music_store.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public void register(RegisterRequest request) {
+        if (userRepository.existsByEmail(request.email())) {
+            throw new IllegalArgumentException("Email already in use");
+        }
+
+        User user = User.builder()
+                .email(request.email())
+                .password(passwordEncoder.encode(request.password()))
+                .role(Role.USER)
+                .build();
+
+        userRepository.save(user);
+    }
+}
