52c98926f1ccc006b816d9a4add5a23e1306f108
Adrian <112898743+adrian-0316@users.noreply.github.com>
2026-01-13 20:14:33 +0300

    security updated

---
+++ src/main/java/com/example/music_store/security/JwtService.java
@@ -0,0 +1,36 @@
+package com.example.music_store.security;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+
+@Service
+public class JwtService {
+
+    private static final String SECRET =
+            "very-secret-key-very-secret-key-very-secret-key";
+    private static final long EXPIRATION = 1000 * 60 * 60; // 1 час
+
+    private final Key key = Keys.hmacShaKeyFor(SECRET.getBytes());
+
+    public String generateToken(String email) {
+        return Jwts.builder()
+                .subject(email)
+                .issuedAt(new Date())
+                .expiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .signWith(key)
+                .compact();
+    }
+
+    public String extractEmail(String token) {
+        return Jwts.parser()
+                .verifyWith(key)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+}
