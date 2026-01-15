695d0c03b0b5594fcb3a2ba83d0b3f8671142e97
Adrian <112898743+adrian-0316@users.noreply.github.com>
2026-01-15 11:15:44 +0300

    service updated

--- src/main/java/com/example/music_store/service/UserService.java
+++ src/main/java/com/example/music_store/service/UserService.java
@@ -1,9 +1,11 @@
 package com.example.music_store.service;
 
+import com.example.music_store.dto.LoginRequest;
 import com.example.music_store.dto.RegisterRequest;
 import com.example.music_store.entity.Role;
 import com.example.music_store.entity.User;
 import com.example.music_store.repository.UserRepository;
+import com.example.music_store.security.JwtService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -14,6 +16,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtService jwtService;
 
     public void register(RegisterRequest request) {
         if (userRepository.existsByEmail(request.email())) {
@@ -27,5 +30,15 @@ public class UserService {
                 .build();
 
         userRepository.save(user);
+    }
+    public String login(LoginRequest request) {
+        User user = userRepository.findByEmail(request.email())
+                .orElseThrow(() -> new IllegalArgumentException("Invalid credentials"));
+
+        if (!passwordEncoder.matches(request.password(), user.getPassword())) {
+            throw new IllegalArgumentException("Invalid credentials");
+        }
+
+        return jwtService.generateToken(user.getEmail());
     }
 }
