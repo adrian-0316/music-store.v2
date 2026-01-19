6dee00dd59eb58cbb56aadc0d1c1fefc0e8319c7
Adrian <112898743+adrian-0316@users.noreply.github.com>
2026-01-19 14:12:13 +0300

    controller updated

--- src/main/java/com/example/music_store/controller/AuthController.java
+++ src/main/java/com/example/music_store/controller/AuthController.java
@@ -1,5 +1,7 @@
 package com.example.music_store.controller;
 
+import com.example.music_store.dto.AuthResponse;
+import com.example.music_store.dto.LoginRequest;
 import com.example.music_store.dto.RegisterRequest;
 import com.example.music_store.service.UserService;
 import jakarta.validation.Valid;
@@ -18,5 +20,11 @@ public class AuthController {
     @ResponseStatus(HttpStatus.CREATED)
     public void register(@RequestBody @Valid RegisterRequest request) {
         userService.register(request);
+    }
+
+    @PostMapping("/login")
+    public AuthResponse login(@RequestBody @Valid LoginRequest request) {
+        String token = userService.login(request);
+        return new AuthResponse(token);
     }
 }
