239c937f1a0b38a76a3f6906083edf3859ed9e91
Adrian <112898743+adrian-0316@users.noreply.github.com>
2026-01-20 11:57:12 +0300

    security updated

---
+++ src/main/java/com/example/music_store/security/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package com.example.music_store.security;
+
+import com.example.music_store.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.*;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email)
+                .map(user -> User.builder()
+                        .username(user.getEmail())
+                        .password(user.getPassword())
+                        .roles(user.getRole().name())
+                        .build())
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+}
