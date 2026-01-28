9bc9cd730f4bb747529f38c2ec90c3bf372ffb3f
Adrian <112898743+adrian-0316@users.noreply.github.com>
2026-01-28 03:29:25 +0300

    security updated

---
+++ src/main/java/com/example/music_store/entity/Category.java
@@ -0,0 +1,17 @@
+package com.example.music_store.entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+}
